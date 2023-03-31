c16aaa9615ab9d6e33cc62b89ebbc41f336e760c
GvantsaMaisuradze <gvantsamaisuradze14@gmail.com>
2023-03-31 11:00:27 +0400

    full website commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     content: ["./dist/*.{html,js}"]
   },
   theme: {
+    screens:{
+      sm:'360px',
+      'md': '768pxpx',
+      lg: ' 1128px',
+      xl: '1312px'
+    },
     extend: {
       colors:{
         primary:'#FF3B30',
@@ -11,7 +17,8 @@ module.exports = {
         small:'#4A5468',
         background1: '#EDF0F7',
         background2: '#E2E7F0',
-        white:'#FFFFFF'
+        white:'#FFFFFF',
+        darkGrey:'#4A5468'
       },
       fontFamily:{
         sans:['FiraGO', 'sans-serif']
